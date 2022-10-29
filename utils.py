9bdd01fe83f3ad93c17d3a11acdd43ef74a62432
mschi <mschiffke@hotmail.de>
2022-10-29 20:37:58 +0200

    working

--- utils.py
+++ utils.py
@@ -1,25 +1,26 @@
 from keras.preprocessing.image import ImageDataGenerator
 from numpy import ndarray
 
-def createImageSegmentationGenerator(images: ndarray, masks: ndarray, augmentation_dict: dict, batch_size: int):
+def createImageSegmentationGenerator(images, masks, batch_size: int):
         """
         Normalize images and encode all existing mask pixel values to 0 and 1 with a treshhold of 0.5
         """
         image_gen = ImageDataGenerator()
         mask_gen = ImageDataGenerator()
 
-        image_gen = image_gen.flow(images, batch_size=batch_size, shuffle=False)
-        mask_gen = mask_gen.flow(masks, batch_size=batch_size, shuffle=False)
+        image_generator = image_gen.flow(x=images, y=masks, batch_size=batch_size, shuffle=True,seed=1)
+        #mask_generator = mask_gen.flow(masks, batch_size=batch_size, shuffle=False, seed=1)
 
-        return_gen = zip(image_gen, mask_gen)
+        #return_gen = zip(image_generator, mask_generator)
 
-        for (img, mask) in return_gen:
+        for (img, mask) in image_generator:
                 img, mask = normalize_and_diagnose(img, mask)
                 yield img, mask
 
 
 def normalize_and_diagnose(img, mask):
-    mask = mask / 255
-    mask[mask > 0.5] = 1
-    mask[mask <= 0.5] = 0
-    return img, mask
+        img = img/255
+        mask = mask / 255
+        mask[mask > 0.5] = 1
+        mask[mask <= 0.5] = 0
+        return img, mask
